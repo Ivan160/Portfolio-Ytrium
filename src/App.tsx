3a02deabd814f89c9759fc31140dbd3621a5cdd6
Ivan160 <dfyzdfyz16@gmail.com>
2020-05-10 01:04:49 +1000

    feat(navbar): created navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, Redirect } from "react-router-dom";
 import './App.scss';
 
 import Navbar from './components/Navbar/Navbar';
 import { Home } from "./components/Content";
+import Logo from "./components/Logo/Logo";
 
-const App = () => (
-    <>
-        <Navbar/>
-        <div className={'content'}>
-            <Switch>
-                <Route path="/" component={Home}/>
-                <Route path="*" render={() => (<Redirect to="/"/>)}/>
-            </Switch>
-        </div>
-    </>
-);
+const App = () => {
+
+    const [ isNavVisible, setNavVisible ] = useState(true);
+
+    const toggleNavVisible = (): void => isNavVisible ? setNavVisible(false) : setNavVisible(true);
+
+    return (
+        <>
+            <Logo isNavVisible={isNavVisible} toggleNavVisible={toggleNavVisible}/>
+            <Navbar isNavVisible={isNavVisible} toggleNavVisible={toggleNavVisible}/>
+            <div className={`content ${!isNavVisible && 'content-full'}`}>
+                <Switch>
+                    <Route path="/" component={Home}/>
+                    <Route path="*" render={() => (<Redirect to="/"/>)}/>
+                </Switch>
+            </div>
+        </>
+    );
+};
 
 export default App;
