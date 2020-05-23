06fdaf34737a7a0569fc6df15935746c539b30b9
Ivan160 <dfyzdfyz16@gmail.com>
2020-05-23 21:25:21 +1000

    feat(home page): adding animation (background and logo)

--- src/App.tsx
+++ src/App.tsx
@@ -8,125 +8,128 @@ import { Home } from "./components/Content";
 import Logo from "./components/Logo/Logo";
 
 const App: FC = () => {
-    const navWidth: number = 62;
-    const menuWidth: number = 250;
-    const resize = useRef<HTMLSpanElement>(null);
-    const content = useRef<HTMLDivElement>(null);
-    const presentation = useRef<HTMLDivElement>(null);
+   const navWidth: number = 62;
+   const contentRadius: number = 40;
+   const menuWidth: number = 250;
+   const resize = useRef<HTMLSpanElement>(null);
+   const content = useRef<HTMLDivElement>(null);
+   const presentation = useRef<HTMLDivElement>(null);
 
-    const [ isHold, setHold ] = useState<boolean>(false);
+   const [ isHold, setHold ] = useState<boolean>(false);
 
-    const [ spanPosition, setSpanPosition ] = useState<number>(navWidth);
-    const [ presentPosition, setPresentPosition ] = useState<number>(0);
+   const [ spanPosition, setSpanPosition ] = useState<number>(navWidth);
+   const [ presentPosition, setPresentPosition ] = useState<number>(0);
 
-    const [ margin, setMargin ] = useState<number>(navWidth);
-    const [ translate, setTranslate ] = useState<number>(0);
+   const [ margin, setMargin ] = useState<number>(navWidth);
+   const [ translate, setTranslate ] = useState<number>(0);
 
-    useEffect(() => {
-        anime({
-            targets: presentation.current,
-            width: {
-                value: `${presentPosition}px`,
-                delay: isHold ? 0 : 1000,
-                duration: 600
+   useEffect(() => {
+      anime({
+         targets: presentation.current,
+         width: {
+            value: `${presentPosition}px`,
+            delay: isHold ? 0 : 1000,
+            duration: 600
+         },
+         opacity: isHold ? 1 : [
+            {
+               value: 1,
+               duration: 180,
             },
-            opacity: isHold ? 1 : [
-                {
-                    value: 1,
-                    duration: 180,
-                },
-                {
-                    value: 0,
-                    duration: 180
-                }
-            ],
-            easing: 'easeOutQuart',
-        });
-    }, [ isHold, presentPosition ]);
+            {
+               value: 0,
+               duration: 180
+            }
+         ],
+         easing: 'easeOutQuart',
+      });
+   }, [ isHold, presentPosition ]);
 
-    const mouseMove = useCallback((event: MouseEvent) => {
-        const mouseMoveX: number = event.pageX;
-        if (mouseMoveX >= 0 && mouseMoveX <= menuWidth) {
-            // @ts-ignore
-            resize.current.style.transform = `translate(${mouseMoveX}px, -50%)`
-            if (mouseMoveX > navWidth + 50) setPresentPosition(menuWidth);
-            else if (mouseMoveX < navWidth - 20) setPresentPosition(0);
-            else setPresentPosition(navWidth);
-        }
-    }, []);
+   const mouseMove = useCallback((event: MouseEvent) => {
+      const mouseMoveX: number = event.pageX;
+      if (mouseMoveX >= 0 && mouseMoveX <= menuWidth) {
+         // @ts-ignore
+         resize.current.style.transform = `translate(${mouseMoveX}px, -50%)`
+         if (mouseMoveX > navWidth + 50) setPresentPosition(menuWidth);
+         else if (mouseMoveX < navWidth - 20) setPresentPosition(0);
+         else setPresentPosition(navWidth);
+      }
+   }, []);
 
-    const mouseDown = useCallback((event: MouseEvent) => {
-        event.preventDefault();
-        if (event.target !== resize.current) return;
-        document.body.style.cursor = 'ew-resize';
-        setSpanPosition(spanPosition - 1);
-        setHold(true);
-        setPresentPosition(spanPosition);
-        document.addEventListener('mousemove', mouseMove);
-    }, [ spanPosition, mouseMove ]);
+   const mouseDown = useCallback((event: MouseEvent) => {
+      event.preventDefault();
+      if (event.target !== resize.current) return;
+      document.body.style.cursor = 'ew-resize';
+      setSpanPosition(spanPosition - 1);
+      setHold(true);
+      setPresentPosition(spanPosition);
+      document.addEventListener('mousemove', mouseMove);
+   }, [ spanPosition, mouseMove ]);
 
-    const mouseUp = useCallback(() => {
-        if (!isHold) return;
-        document.removeEventListener('mousemove', mouseMove);
-        document.body.style.cursor = 'default';
-        setHold(false);
-        setSpanPosition(presentPosition);
-        if (presentPosition > navWidth) {
-            setMargin(navWidth);
-            setTranslate(presentPosition - navWidth);
-        } else if (presentPosition < navWidth) {
-            setMargin(0);
-            setTranslate(0);
-        } else {
-            setMargin(navWidth);
-            setTranslate(0);
-        }
-        setPresentPosition(0);
-    }, [ presentPosition, isHold, mouseMove ]);
+   const mouseUp = useCallback(() => {
+      if (!isHold) return;
+      document.removeEventListener('mousemove', mouseMove);
+      document.body.style.cursor = 'default';
+      setHold(false);
+      setSpanPosition(presentPosition);
+      if (presentPosition > navWidth) {
+         setMargin(navWidth);
+         setTranslate(presentPosition - navWidth);
+      } else if (presentPosition < navWidth) {
+         setMargin(0);
+         setTranslate(0);
+      } else {
+         setMargin(navWidth);
+         setTranslate(0);
+      }
+      setPresentPosition(0);
+   }, [ presentPosition, isHold, mouseMove ]);
 
-    useEffect(() => {
-        document.body.addEventListener('mousedown', mouseDown);
-        return () => document.body.removeEventListener('mousedown', mouseDown);
-    }, [ mouseDown ]);
+   useEffect(() => {
+      document.body.addEventListener('mousedown', mouseDown);
+      return () => document.body.removeEventListener('mousedown', mouseDown);
+   }, [ mouseDown ]);
 
-    useEffect(() => {
-        document.body.addEventListener('mouseup', mouseUp);
-        return () => document.body.removeEventListener('mouseup', mouseUp);
-    }, [ mouseUp ]);
+   useEffect(() => {
+      document.body.addEventListener('mouseup', mouseUp);
+      return () => document.body.removeEventListener('mouseup', mouseUp);
+   }, [ mouseUp ]);
 
-    useEffect(() => {document.body.style.cursor = 'default'}, []);
+   useEffect(() => {
+      document.body.style.cursor = 'default'
+   }, []);
 
-    const doubleClick = useCallback((e: React.MouseEvent<HTMLSpanElement>) => {
-        e.preventDefault();
-        setSpanPosition(navWidth);
-        setMargin(navWidth);
-        setTranslate(0);
-    }, []);
+   const doubleClick = useCallback((e: React.MouseEvent<HTMLSpanElement>) => {
+      e.preventDefault();
+      setSpanPosition(navWidth);
+      setMargin(navWidth);
+      setTranslate(0);
+   }, []);
 
-    return (
-        <>
-            <Logo isOpenMenu={translate} isOpenNav={margin}/>
+   return (
+      <>
+         <Logo isOpenMenu={translate} isOpenNav={margin}/>
 
-            <div className="presentation" ref={presentation}/>
+         <div className="presentation" ref={presentation}/>
 
-            <Navbar isOpenMenu={translate}/>
-            <span className={'resize'} ref={resize}
-                  onDoubleClick={doubleClick}
-                  style={{ transform: `translate(${spanPosition}px, -50%)` }}/>
-            <div className={`content`} ref={content}
-                 style={{
-                     marginLeft: `${margin}px`,
-                     paddingRight: `${margin}px`,
-                     transform: `translateX(${translate}px)`,
-                     borderRadius: `${margin === 0 ? 0 : 40}px`
-                 }}>
-                <Switch>
-                    <Route path="/" render={() => (<Home isOpenNav={margin}/>)}/>
-                    <Route path="*" render={() => (<Redirect to="/"/>)}/>
-                </Switch>
-            </div>
-        </>
-    );
+         <Navbar isOpenMenu={translate}/>
+         <span className={'resize'} ref={resize}
+               onDoubleClick={doubleClick}
+               style={{ transform: `translate(${spanPosition}px, -50%)` }}/>
+         <div className={`content`} ref={content}
+              style={{
+                 marginLeft: `${margin}px`,
+                 //paddingRight: `${margin}px`,
+                 transform: `translateX(${translate}px)`,
+                 borderRadius: `${margin === 0 ? 0 : `${contentRadius}px 0 0 ${contentRadius}px`}`
+              }}>
+            <Switch>
+               <Route path="/" render={() => (<Home isOpenNav={margin}/>)}/>
+               <Route path="*" render={() => (<Redirect to="/"/>)}/>
+            </Switch>
+         </div>
+      </>
+   );
 };
 
 export default App;
---
+++ src/components/Content/Home/TextSkills/TextSkills.tsx
@@ -0,0 +1,87 @@
+import React, { FC, useEffect, useRef, useState, useCallback } from "react";
+import style from "./TextSkills.module.scss";
+
+type Props = {}
+
+const TextSkills: FC<Props> = (props) => {
+   const text = useRef<HTMLDivElement>(null);
+   const requestRef = useRef<any>();
+   const phrases: Array<string> = [
+      'Frontend',
+      'React',
+      'Backend',
+      'Express',
+      'Freelancer',
+      'Designer'
+   ];
+
+   const [ counter, setCounter ] = useState<number>(0);
+
+   const update = useCallback((queue: Array<any>) => {
+      const chars = '!<>-_\\/[]{}—=+*^?#________';
+      let frame: number = 0;
+
+      const animationFrame = () => {
+         let output: string = '';
+         let complete: number = 0;
+
+         for (let i = 0; i < queue.length; i++) {
+            let { from, to, start, end, char } = queue[i];
+            if (frame >= end) {
+               complete++;
+               output += to;
+            } else if (frame >= start) {
+               if (!char || Math.random() < 0.28) {
+                  char = chars[Math.floor(Math.random() * chars.length)];
+                  queue[i].char = char;
+               }
+               output += `<span>${char}</span>`;
+            } else {
+               output += from;
+            }
+         }
+
+         // @ts-ignore
+         text.current.innerHTML = output;
+         if (complete === queue.length) {
+            cancelAnimationFrame(requestRef.current);
+            frame = 0;
+            setCounter((counter + 1) % phrases.length);
+         } else {
+            requestRef.current = requestAnimationFrame(animationFrame);
+            frame++;
+         }
+      };
+      animationFrame();
+   }, [ counter, phrases ]);
+
+   const setText = useCallback((newText: string) => {
+      let queue: Array<any> = [];
+      // @ts-ignore
+      const oldText = text.current.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      for (let i = 0; i < length; i++) {
+         const from = oldText[i] || '';
+         const to = newText[i] || '';
+         const start = Math.floor(Math.random() * 40);
+         const end = start + Math.floor(Math.random() * 40);
+         queue.push({ from, to, start, end });
+      }
+      update(queue);
+   }, [ update ])
+
+   useEffect(() => {
+      const timeout = setTimeout(() => setText(phrases[counter]), 3000);
+      return () => {
+         clearTimeout(timeout);
+         cancelAnimationFrame(requestRef.current);
+      }
+   }, [ counter, setText, phrases ]);
+
+   return (
+      <div className={style.skill_text}>
+         <div className={style.text} ref={text}>|</div>
+      </div>
+   );
+};
+export default TextSkills;
